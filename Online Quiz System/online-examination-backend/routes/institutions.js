2b149dc3dc370c96a924dc282d24a6cf72e998ee
vaish0403 <57799996+vaish0403@users.noreply.github.com>
2021-06-16 13:30:08 +0530

    Delete Online Quiz System directory

--- Online Quiz System/online-examination-backend/routes/institutions.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const controllers = require('../controllers/institutionController');
-
-router.get('/listOfInstitutions', controllers.listOfInstitutions);
-
-module.exports = router;
